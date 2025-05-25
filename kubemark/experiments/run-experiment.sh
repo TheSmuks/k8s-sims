009b9da9c8c9e71cf3e1411f02d9ffbefa98915f
TheSmuks <therealsmuks@gmail.com>
2025-05-25 21:59:13 +0200

    Added WIP kubemark data collection script

--- kubemark/experiments/run-experiment.sh
+++ kubemark/experiments/run-experiment.sh
@@ -1,18 +1,90 @@
 #!/bin/bash
-kind create cluster --config=kind-config.yaml --name testing --image kindest/node:v1.29.0
-kind get kubeconfig --name testing > ./config
+
+CLUSTER_NAME="testing"
+CGROUP_BASE="/sys/fs/cgroup/system.slice"
+
+function track_containers(){
+    NODE_IDS=($(docker ps --no-trunc -aqf "name=$CLUSTER_NAME"))
+    POLL_TIMEOUT=0.1
+    MAX_MEMORY_MEASUREMENTS=()
+    for ((i=0; i<${#NODE_IDS[@]}; i++)); do
+        MAX_MEMORY_MEASUREMENTS[i]=-1
+    done
+	RUN_CONDITION="true"
+	trap 'RUN_CONDITION=false' SIGINT
+	while [ $RUN_CONDITION = "true" ]; do
+        for ((i=0; i<${#NODE_IDS[@]}; i++)); do
+            if [ -d "$CGROUP_BASE/docker-${NODE_IDS[i]}.scope" ]; then
+                CURRENT_MEMORY_MEASUREMENT=$(cat "$CGROUP_BASE/docker-${NODE_IDS[i]}.scope/memory.current")
+		        if [ $CURRENT_MEMORY_MEASUREMENT -gt ${MAX_MEMORY_MEASUREMENTS[i]} ]; then
+			        MAX_MEMORY_MEASUREMENTS[i]=$CURRENT_MEMORY_MEASUREMENT
+		        fi
+                CPU_TIMES=($(awk 'NR<=3 {printf "%.2f ", $2/1000000}' "$CGROUP_BASE/docker-${NODE_IDS[i]}.scope/cpu.stat"))
+                CPU_MEASUREMENTS=()                
+                for ((j=0; j<3; j++)); do
+                    CPU_MEASUREMENTS[j]+=${CPU_TIMES[j]}
+                done
+            else
+                RUN_CONDITION="false"
+                break
+            fi
+        done
+		sleep $POLL_TIMEOUT
+	done
+    END_TIME=$(date +%s)
+    TOTAL_MEM=0
+    for ((i=0; i<${#NODE_IDS[@]}; i++)); do
+        TOTAL_MEM=$(($TOTAL_MEM + ${MAX_MEMORY_MEASUREMENTS[i]}))
+    done
+    printf "%d" $(($END_TIME - $2)) >>$1
+    for ((i=0; i<3; i++)); do
+        printf "|%.2f" ${CPU_MEASUREMENTS[i]} >> $1
+    done
+	printf "|%.2f\n" $(($TOTAL_MEM / 1024 / 1024 / 1024)) >> $1
+	exit 0
+}
+echo "run_time|total_cpu_seconds|user_cpu_seconds|system_cpu_seconds|memory_peak_gb" > "$1"
+START_TIME=$(date +%s)
+kind create cluster --config=kind-config.yaml --name $CLUSTER_NAME --image kindest/node:v1.29.0
+track_containers $1 $START_TIME &
+POLL_PID=$!
+kind get kubeconfig --name $CLUSTER_NAME > ./config
 sed -i 's|server: https://127.0.0.1:[0-9]\+|server: https://kubernetes.default.svc:443|' ./config
-kubectl config use-context kind-testing
+kubectl config use-context kind-$CLUSTER_NAME
 kubectl create ns kubemark
 kubectl create secret generic kubeconfig \
 --type=Opaque --namespace=kubemark \
 --from-file=kubelet.kubeconfig=config \
 --from-file=kubeproxy.kubeconfig=config
-#kubectl create -f hollow-nodes.yml
-#while [ $(kubectl get pods -n kubemark | grep "Running" | wc -l) -lt 1213 ] 
-#do 
-#	RUNNING_CONTAINERS=$(kubectl get pods -n kubemark | grep "Running" | wc -l) 
-#	echo -n -e " $RUNNING_CONTAINERS/1213 pods running...\r" 
-#	sleep 1
-#done; 
-#
+kubectl create -f hollow-nodes.yml
+echo "Creating pods"
+kubectl create -f pods.yaml -n kubemark
+FAILURE_FOUND="false"
+PENDING_PODS_COUNT=$(kubectl get pods --field-selector=status.phase=Pending -n kubemark --no-headers | wc -l)
+while [ $PENDING_PODS_COUNT -gt 0 ]
+do
+    if [ $FAILURE_FOUND = "true" ]; then
+        break    
+    fi
+    PENDING_PODS=$(kubectl get pods --field-selector=status.phase=Pending -n kubemark -o jsonpath='{.items[*].metadata.name}')
+    FAILURE_COUNT=0    
+    for pod_name in $PENDING_PODS; do
+        FAILURE_CHECK=$(kubectl get events -n kubemark --field-selector involvedObject.name="$pod_name" | \
+                       grep "FailedScheduling" | \
+                       grep -E "(Insufficient cpu|Insufficient memory|No preemption victims found)")
+        
+        if [ -n "$FAILURE_CHECK" ]; then
+            FAILURE_COUNT=$((FAILURE_COUNT+1))
+            if [ $FAILURE_COUNT -ge $PENDING_PODS_COUNT ]; then
+                FAILURE_FOUND="true"  
+                break          
+            fi
+        fi
+    done
+    echo -ne "Pending pods: $PENDING_PODS_COUNT\r"
+    sleep 1
+    PENDING_PODS_COUNT=$(kubectl get pods --field-selector=status.phase=Pending -n kubemark --no-headers | wc -l)
+done;
+#kubectl get pods -A
+kind delete cluster --name $CLUSTER_NAME
+kill -s SIGINT $POLL_PID > /dev/null 2>&1;
--- utils/cgroups-util.sh
+++ utils/cgroups-util.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
 
-was_last_command_success(){
+RUNS=3
+USING_DOCKER="false"
+
+while getopts 'h?p:s:r:d' opt; do
+	case "$opt" in
+	p)
+		PROGRAM_NAME="$OPTARG"
+		;;
+	s)
+		SCRIPT_PATH="$OPTARG"
+		;;
+	r)
+		RUNS="$OPTARG"
+		;;
+
+	d)
+		USING_DOCKER="true"
+		;;
+
+	h)
+		echo "Usage: $(basename $0) <-p program-name> <-s script-path> [-d]  [-r runs]"
+		exit 0
+		;;
+
+	:)
+		echo -e "option requires an argument.\nUsage: $(basename $0) <-p program-name> <-s script-path> [-d]  [-r runs]"
+		exit 1
+		;;
+
+	?)
+		echo -e "Invalid command option.\nUsage: $(basename $0) <-p program-name> <-s script-path> [-d] [-r runs]"
+		exit 1
+		;;
+	esac
+done
+shift "$(($OPTIND - 1))"
+exit 0
+
+was_last_command_success() {
 	if [ $? -ne 0 ]; then
 		echo "$1"
 		exit 1
 	fi
 }
 
-poll_memory_peak(){
+poll_memory_peak() {
 	POLL_TIMEOUT=0.1
 	MAX_MEMORY_MEASUREMENT=-1
 	RUN_CONDITION=true
@@ -19,16 +57,13 @@ poll_memory_peak(){
 		fi
 		sleep $POLL_TIMEOUT
 	done
-	printf "|%.2f\n" $(($MAX_MEMORY_MEASUREMENT/1024/1024/1024)) >> $2
+	printf "|%.2f\n" $(($MAX_MEMORY_MEASUREMENT / 1024 / 1024 / 1024)) >>$2
 	exit 0
 }
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <group_name> <program> [runs]"
-    exit 1
-fi
+fetch_data(){
 
-RUNS=3
+}
 
 if [ "$#" -gt 2 ]; then
 	RUNS=$3
@@ -36,11 +71,10 @@ fi
 
 CURRENT_RUNS=0
 CGROUP_BASE="/sys/fs/cgroup/"
-RUN_OUT="./run.csv"
-echo "run_time|cpu_seconds_time|memory_peak" > "$RUN_OUT" 
-while [ $CURRENT_RUNS -lt $RUNS ]
-do
-	CGROUP_NAME="$1-$CURRENT_RUNS"
+RUN_OUT="$(pwd)/run-$PROGRAM_NAME.csv"
+echo "run_time|cpu_seconds_time|memory_peak" >"$RUN_OUT"
+while [ $CURRENT_RUNS -lt $RUNS ]; do
+	CGROUP_NAME="$PROGRAM_NAME-$CURRENT_RUNS"
 	sudo cgcreate -g memory,cpu:/$CGROUP_NAME
 	was_last_command_success "Failed to create cgroup"
 	echo "$CGROUP_NAME cgroup created"
@@ -48,17 +82,16 @@ do
 	POLL_PID=$!
 	echo "Poll process started: $POLL_PID"
 	START_TIME=$(date +%s)
-	sudo cgexec -g memory,cpu:/$CGROUP_NAME $2
+	sudo cgexec -g memory,cpu:/$CGROUP_NAME $SCRIPT_PATH
 	was_last_command_success "Failed to run program"
 	END_TIME=$(date +%s)
-	printf "%d" $(($END_TIME-$START_TIME)) >> "$RUN_OUT"
-	awk 'NR==1 {printf "|%.2f", $2/1000000}' "$CGROUP_BASE$CGROUP_NAME/cpu.stat" >> "$RUN_OUT"
+	printf "%d" $(($END_TIME - $START_TIME)) >>"$RUN_OUT"
+	awk 'NR==1 {printf "|%.2f", $2/1000000}' "$CGROUP_BASE$CGROUP_NAME/cpu.stat" >>"$RUN_OUT"
 	kill -s SIGINT $POLL_PID
-	sleep 1 
+	sleep 1
 	sudo cgdelete memory,cpu:/$CGROUP_NAME
 	was_last_command_success "Failed to delete cgroup $CGROUP_NAME"
 	echo "cgroup $CGROUP_NAME deleted"
-	CURRENT_RUNS=$(($CURRENT_RUNS+1))
+	CURRENT_RUNS=$(($CURRENT_RUNS + 1))
 	echo -e -n "$CURRENT_RUNS/$RUNS completed runs...\r"
 done
-
